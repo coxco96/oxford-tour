aa76a8b3a01a3a7f230a2606e2be18f7fdb06954
Court Cox <82842603+coxco96@users.noreply.github.com>
2021-12-14 23:48:10 -0500

    add image class ready for change on click

--- oxfordLocations.js
+++ oxfordLocations.js
@@ -1,109 +1,190 @@
 oxfordLocations = {
-    id: {
-
-      1: {
-        name: "Magdalen College",
-        latlng: [
-          [-464.3, 635.7],
-          [-224.8, 945.7]
-        ],
-        description: "",
-        mapNum: 1
-      },
-
-      2: {
-        name: "All Souls",
-        latlng: [
-          [-303.6, 467.4],
-          [-183.8, 622.4]
-        ],
-        description: "",
-        mapNum: 7
-      },
-
-      3: {
-        name: "Radcliffe Library",
-        latlng: [
-          [-295.5, 430.9],
-          [-191.3, 565.8]
-        ],
-        description: "",
-        mapNum: 11
-      },
-
-      4: {
-        name: "Bodleian D.",
-        latlng: [
-          [-282.2, 448],
-          [-162.5, 603]
-        ],
-        description: "",
-        mapNum: 12
-      },
-
-      5: {
-        name: "The Market",
-        latlng: [
-          [-288.7, 236.6],
-          [-145.5, 421.2]
-        ],
-        description: "",
-        mapNum: 16
-      },
-
-      6: {
-        name: "Oxford Castle",
-        latlng: [
-          [-222.3, 36],
-          [-97.6, 196.6]
-        ],
-        description: "",
-        mapNum: 22
-      },
-
-      7: {
-        name: "",
-       
-        latlng: [
-          // [-222.3, 36],
-          // [-97.6, 196.6]
-        ],
-        description: "",
-        mapNum: ""
-      },
-
-      8: {
-        name: "Christ Church College",
-        latlng: [
-          [-334.3, 144.4],
-          [-225.7, 304.4]
-        ],
-        description: "",
-        mapNum: 28
-      },
-
-      9: {
-        name: "",
-       
-        latlng: [
-          // [-222.3, 36],
-          // [-97.6, 196.6]
-        ],
-        description: "",
-        mapNum: ""
-      },
-      
-      10: {
-        name: "",
-       
-        latlng: [
-          // [-222.3, 36],
-          // [-97.6, 196.6]
-        ],
-        description: "",
-        mapNum: ""
-      },
+  id: {
 
 
+    1: {
+      name: "Oxford",
+      latlng: [
+        [-504.3, 106.2],
+        [-70.07, 746.07]
+      ],
+      description: "",
+      mapNum: 1,
+      image: {
+        src: "images/id1.jpg",
+        alt: "Magdalen College, light brick historic building surrounded by bright greenery",
+        credit: "https://www.britainexpress.com/cities/oxford/magdalen.htm"
+      }
+    },
+
+    2: {
+      name: "All Souls",
+      latlng: [
+        [-303.6, 467.4],
+        [-183.8, 622.4]
+      ],
+      description: "",
+      mapNum: 7,
+      image: {
+        src: "",
+        alt: "",
+        credit: ""
+      }
+    },
+
+    3: {
+      name: "Radcliffe Library",
+      latlng: [
+        [-295.5, 430.9],
+        [-191.3, 565.8]
+      ],
+      description: "",
+      mapNum: 11,
+      image: {
+        src: "",
+        alt: "",
+        credit: ""
+      }
+    },
+
+    4: {
+      name: "Bodleian D.",
+      latlng: [
+        [-282.2, 448],
+        [-162.5, 603]
+      ],
+      description: "",
+      mapNum: 12,
+      image: {
+        src: "",
+        alt: "",
+        credit: ""
+      }
+    },
+
+    5: {
+      name: "The Market",
+      latlng: [
+        [-288.7, 236.6],
+        [-145.5, 421.2]
+      ],
+      description: "",
+      mapNum: 16,
+      image: {
+        src: "",
+        alt: "",
+        credit: ""
+      }
+    },
+
+    6: {
+      name: "Oxford Castle",
+      latlng: [
+        [-222.3, 36],
+        [-97.6, 196.6]
+      ],
+      description: "",
+      mapNum: 22,
+      image: {
+        src: "",
+        alt: "",
+        credit: ""
+      }
+    },
+
+    7: {
+      name: "Christ Church College",
+      latlng: [
+        [-334.3, 144.4],
+        [-225.7, 304.4]
+      ],
+      description: "",
+      mapNum: 28,
+      image: {
+        src: "",
+        alt: "",
+        credit: ""
+      }
+    },
+
+    8: {
+      name: "Corpus Christi College",
+      latlng: [
+        [-371.2, 280.4],
+        [-262.6, 431.7]
+      ],
+      description: "",
+      mapNum: 31,
+      image: {
+        src: "",
+        alt: "",
+        credit: ""
+      }
+    },
+
+    9: {
+      name: "New Printing Office",
+      latlng: [
+        [-180.1, 279.9],
+        [-71.5, 431.2]
+      ],
+      description: "",
+      mapNum: 41,
+      image: {
+        src: "",
+        alt: "",
+        credit: ""
+      }
+    },
+
+    10: {
+      name: "Martyrs Monument",
+
+      latlng: [
+        [-201.8, 376],
+        [-119.6, 490.7]
+      ],
+      description: "",
+      mapNum: 52,
+      image: {
+        src: "",
+        alt: "",
+        credit: ""
+      }
+    },
+
+    11: {
+      name: "Ashmolean Museum",
+
+      latlng: [
+        [-252, 454],
+        [-169.7, 568.5]
+      ],
+      description: "",
+      mapNum: 58,
+      image: {
+        src: "",
+        alt: "",
+        credit: ""
+      }
+    },
+
+    12: {
+      name: "The Theatre",
+
+      latlng: [
+        [-247.9, 478.7],
+        [-165.6, 593.4]
+      ],
+      description: "",
+      mapNum: 58,
+      image: {
+        src: "",
+        alt: "",
+        credit: ""
+      }
     }
+
+
   }
+}
