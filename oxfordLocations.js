f405e5e2a403f68d04d66147214ae33a2c2c9ad7
Court Cox <82842603+coxco96@users.noreply.github.com>
2021-12-14 21:34:19 -0500

    add all point stops

---
+++ oxfordLocations.js
@@ -0,0 +1,109 @@
+oxfordLocations = {
+    id: {
+
+      1: {
+        name: "Magdalen College",
+        latlng: [
+          [-464.3, 635.7],
+          [-224.8, 945.7]
+        ],
+        description: "",
+        mapNum: 1
+      },
+
+      2: {
+        name: "All Souls",
+        latlng: [
+          [-303.6, 467.4],
+          [-183.8, 622.4]
+        ],
+        description: "",
+        mapNum: 7
+      },
+
+      3: {
+        name: "Radcliffe Library",
+        latlng: [
+          [-295.5, 430.9],
+          [-191.3, 565.8]
+        ],
+        description: "",
+        mapNum: 11
+      },
+
+      4: {
+        name: "Bodleian D.",
+        latlng: [
+          [-282.2, 448],
+          [-162.5, 603]
+        ],
+        description: "",
+        mapNum: 12
+      },
+
+      5: {
+        name: "The Market",
+        latlng: [
+          [-288.7, 236.6],
+          [-145.5, 421.2]
+        ],
+        description: "",
+        mapNum: 16
+      },
+
+      6: {
+        name: "Oxford Castle",
+        latlng: [
+          [-222.3, 36],
+          [-97.6, 196.6]
+        ],
+        description: "",
+        mapNum: 22
+      },
+
+      7: {
+        name: "",
+       
+        latlng: [
+          // [-222.3, 36],
+          // [-97.6, 196.6]
+        ],
+        description: "",
+        mapNum: ""
+      },
+
+      8: {
+        name: "Christ Church College",
+        latlng: [
+          [-334.3, 144.4],
+          [-225.7, 304.4]
+        ],
+        description: "",
+        mapNum: 28
+      },
+
+      9: {
+        name: "",
+       
+        latlng: [
+          // [-222.3, 36],
+          // [-97.6, 196.6]
+        ],
+        description: "",
+        mapNum: ""
+      },
+      
+      10: {
+        name: "",
+       
+        latlng: [
+          // [-222.3, 36],
+          // [-97.6, 196.6]
+        ],
+        description: "",
+        mapNum: ""
+      },
+
+
+    }
+  }
