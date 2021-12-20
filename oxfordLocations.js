640f1483138e5baa66e1645ed04b1eb4a2861e56
Court Cox <82842603+coxco96@users.noreply.github.com>
2021-12-20 14:03:39 -0500

    update photo credits

--- oxfordLocations.js
+++ oxfordLocations.js
@@ -3,37 +3,38 @@ oxfordLocations = {
 
 
     0: {
-      name: "Oxford",
+      name: `<h1>Welcome to Oxford.</h1><h2>Let's take a tour.</h2>`,
       latlng: [
         [-504.3, 106.2],
         [-70.07, 746.07]
       ],
-      description: "",
-      mapNum: 1,
+      description: 
+      "Oxford is an academic fairytale land brought to life. The University of Oxford is sprawled throughout the city of Oxford. The university is made up of 45 colleges and registered halls, each with their own sort of miniature campus. Lectures happen all over the city, and each of Oxford's colleges has its own library, dining hall, and living quarters. Tutors, as professors are called, often live in community within the college they are part of. <br> The University of Oxford is the second oldest university in the world, with evidence of teaching as early as 1096 (over 900 years ago).<br> However, Oxford wasn't always what it is today. Women were not permitted to study at Oxford until 1878 and could not earn degrees until 1920. It wasn't until 1974 that every college permitted women's admission. Now, though, Oxford has a roughly equal gender spread.<br>The map you see was drawn by cartographer <a href='https://collections.leventhalmap.org/search/commonwealth:3f4632472'>Nathaniel Whittock</a> in 1850. While much has changed in Oxford since then, many of its rich historic sites remain in their same locations, still thiriving in the city's ceaseless thirst for knowledge and understanding.<br>Click the buttons below to take a virtual tour of some of Oxford's historic sites--or, feel free to explore the map on your own using the numbered locations on the bottom of Whittock's map.",
+      mapNum: null,
       image: {
         src: "images/id1.jpg",
-        alt: "",
-        credit: ""
+        alt: "bubble wide lens style photo of Radcliffe Camera library, a concentric rotunda historic building",
+        credit: "Photo by <a href='https://unsplash.com/@benseymour?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText'>Ben Seymour</a> on <a href='https://unsplash.com/s/photos/oxford-university?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText'>Unsplash</a>"
       }
     },
 
     1: {
-      name: "All Souls",
+      name: "All Souls College (#7)",
       latlng: [
         [-303.6, 467.4],
         [-183.8, 622.4]
       ],
-      description: "",
+      description: "All Souls College is unique even within Oxford. There are no undergraduate to be found in the college, and Oxford students are not permitted to step foot inside the college. Members of the college automatically become Fellowss (<a href='https://www.oxfordcityguide.com/oxford-tourist-information/oxford-university/all-souls'>'academics who are full governing members of the College'</a>). The entrance exam has been called the hardest exam in the world. Oxford graduates are invited to take the entrance exam, with only one or two fellows accepted per year.",
       mapNum: 7,
       image: {
         src: "images/id2.jpg",
         alt: "drone perspective of All Souls College old buildings and green courtyard with horizon line visible",
-        credit: "https://artuk.org/visit/venues/all-souls-college-university-of-oxford-7507"
+        credit: "Image found <a href='https://artuk.org/visit/venues/all-souls-college-university-of-oxford-7507'>here</a>."
       }
     },
 
     2: {
-      name: "Radcliffe Library",
+      name: "Radcliffe Library (#11)",
       latlng: [
         [-295.5, 430.9],
         [-191.3, 565.8]
@@ -43,12 +44,12 @@ oxfordLocations = {
       image: {
         src: "images/id3.jpg",
         alt: "concentric tiered grand library",
-        credit: "https://en.wikipedia.org/wiki/Radcliffe_Camera#/media/File:Radcliffe_Camera,_Oxford_-_Oct_2006.jpg"
+        credit: "Image found <a href ='https://en.wikipedia.org/wiki/Radcliffe_Camera#/media/File:Radcliffe_Camera,_Oxford_-_Oct_2006.jpg'>here</a>."
       }
     },
 
     3: {
-      name: "Bodleian D.",
+      name: "Bodleian D. (#12)",
       latlng: [
         [-282.2, 448],
         [-162.5, 603]
@@ -58,12 +59,12 @@ oxfordLocations = {
       image: {
         src: "images/id4.jpg",
         alt: "inside view of Bodleian library with tables in straight line through middle and bookshelves on either side",
-        credit: "https://medium.com/oxford-university/seven-things-you-probably-didnt-know-about-oxford-s-libraries-38ed3748add"
+        credit: "Image found <a href = 'https://medium.com/oxford-university/seven-things-you-probably-didnt-know-about-oxford-s-libraries-38ed3748add' here</a>."
       }
     },
 
     4: {
-      name: "The Market",
+      name: "The Market (#16)",
       latlng: [
         [-288.7, 236.6],
         [-145.5, 421.2]
@@ -73,12 +74,12 @@ oxfordLocations = {
       image: {
         src: "images/id5.jpg",
         alt: "flowers, hall, and shops framed in covered market space",
-        credit: "https://commons.wikimedia.org/wiki/File:Oxford_-_Covered_Market_-_0127.jpg"
+        credit: "Image found <a href='https://commons.wikimedia.org/wiki/File:Oxford_-_Covered_Market_-_0127.jpg'> here</a>."
       }
     },
 
     5: {
-      name: "Oxford Castle",
+      name: "Oxford Castle (#22)",
       latlng: [
         [-222.3, 36],
         [-97.6, 196.6]
@@ -88,12 +89,12 @@ oxfordLocations = {
       image: {
         src: "images/id6.jpg",
         alt: "stone castle and blue sky",
-        credit: "https://www.viator.com/en-PH/tours/Oxford/Oxford-Castle-Unlocked-Entrance-Ticket-Including-Guided-Tour/d5537-31897P1"
+        credit: "Image found <a href='https://www.viator.com/en-PH/tours/Oxford/Oxford-Castle-Unlocked-Entrance-Ticket-Including-Guided-Tour/d5537-31897P1' here</a>."
       }
     },
 
     6: {
-      name: "Christ Church College",
+      name: "Christ Church College (#28)",
       latlng: [
         [-334.3, 144.4],
         [-225.7, 304.4]
@@ -103,12 +104,12 @@ oxfordLocations = {
       image: {
         src: "images/id7.jpg",
         alt: "",
-        credit: "https://commons.wikimedia.org/w/index.php?curid=90518916"
+        credit: "Image found <a href='https://commons.wikimedia.org/w/index.php?curid=90518916'>here</a>."
       }
     },
 
     7: {
-      name: "Corpus Christi College",
+      name: "Corpus Christi College (#31)",
       latlng: [
         [-371.2, 280.4],
         [-262.6, 431.7]
@@ -118,12 +119,12 @@ oxfordLocations = {
       image: {
         src: "images/id8.jpg",
         alt: "dark stone pavement, lighter stone building and a relatively smaller statue/marker",
-        credit: "https://en.wikipedia.org/wiki/Corpus_Christi_College,_Oxford#/media/File:UK-2014-Oxford-Corpus_Christi_College_02.jpg"
+        credit: "Image found <a href='https://en.wikipedia.org/wiki/Corpus_Christi_College,_Oxford#/media/File:UK-2014-Oxford-Corpus_Christi_College_02.jpg'>here</a>."
       }
     },
 
     8: {
-      name: "New Printing Office",
+      name: "New Printing Office (#41)",
       latlng: [
         [-180.1, 279.9],
         [-71.5, 431.2]
@@ -133,12 +134,12 @@ oxfordLocations = {
       image: {
         src: "images/id9.jpg",
         alt: "Straight on view of building that says Oxford Printing Press and with gate entrance in middle",
-        credit: "https://en.wikipedia.org/wiki/Oxford_University_Press#/media/File:Oxford_University_Press_Building_%E2%80%93_Walton_Street.jpg"
+        credit: "Image found <a href='https://en.wikipedia.org/wiki/Oxford_University_Press#/media/File:Oxford_University_Press_Building_%E2%80%93_Walton_Street.jpg'>here</a>."
       }
     },
 
     9: {
-      name: "Martyrs Monument",
+      name: "Martyrs Monument (#52)",
 
       latlng: [
         [-201.8, 376],
@@ -149,12 +150,12 @@ oxfordLocations = {
       image: {
         src: "images/id10.jpg",
         alt: "tall monument with steps at the base, people are sitting on the steps",
-        credit: "https://www.britainexpress.com/cities/oxford/martyrs.htm"
+        credit: "Image found <a href='https://www.britainexpress.com/cities/oxford/martyrs.htm'>here</a>."
       }
     },
 
     10: {
-      name: "Ashmolean Museum",
+      name: "Ashmolean Museum (#58)",
 
       latlng: [
         [-252, 454],
@@ -165,23 +166,23 @@ oxfordLocations = {
       image: {
         src: "images/id11.jpg",
         alt: "museum entrance with flags and people outside",
-        credit: "https://commons.wikimedia.org/w/index.php?curid=77717785"
+        credit: "Image found <a href= 'https://commons.wikimedia.org/w/index.php?curid=77717785'>here</a>."
       }
     },
 
     11: {
-      name: "The Theatre",
+      name: "The Theatre (#59)",
 
       latlng: [
         [-247.9, 478.7],
         [-165.6, 593.4]
       ],
       description: "",
-      mapNum: 58,
+      mapNum: 59,
       image: {
         src: "images/id12.jpg",
         alt: "",
-        credit: "https://en.wikipedia.org/wiki/Sheldonian_Theatre#/media/File:Oxford_Building_Sunset.jpg"
+        credit: "Image found <a href= 'https://en.wikipedia.org/wiki/Sheldonian_Theatre#/media/File:Oxford_Building_Sunset.jpg'>here</a>."
       }
     }
 
